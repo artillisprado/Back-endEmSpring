84a07edc311e07255905e2a07681ff084fe2a828
artillisprado <aadejarde15@gmail.com>
2023-01-28 20:13:48 -0300

    configurando desenvolvimento com mysql

---
+++ src/main/java/com/artillis/urbana/config/devconfig.java
@@ -0,0 +1,28 @@
+package com.artillis.urbana.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.artillis.urbana.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class devconfig {
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbservice.instanciaDB();
+		}
+		return false;
+		
+	}
+}
