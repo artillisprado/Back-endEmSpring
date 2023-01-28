2e5730079edc9815f72cce9e24877b342e4defe1
artillisprado <aadejarde15@gmail.com>
2023-01-28 17:43:47 -0300

    criando tipos enumerados

---
+++ src/main/java/com/artillis/urbana/domain/enums/Status.java
@@ -0,0 +1,37 @@
+package com.artillis.urbana.domain.enums;
+
+public enum Status {
+	ATIVO(0, "ATIVO"), INATIVO(1,"INATIVO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Status(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Status toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for (Status x : Status.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Perfil inválido");
+	}
+	
+
+}
---
+++ src/test/java/com/artillis/urbana/UrbanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artillis.urbana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
